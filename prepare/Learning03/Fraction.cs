08fb3189b4d60969d11fc6996eed65fd4ddcb53c
guifabri <erickscala_19@hotmail.com>
2024-03-25 21:17:04 -0400

    changes

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,39 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int num){
+        _bottom = 1;
+        _top = num;
+    }
+    public Fraction(int num1, int num2){
+        _top = num1;
+        _bottom = num2;
+    }
+    public int GetTop(){
+        return _top;
+    }
+    public void SetTop(int top){
+        _top = top;
+    }
+    public int GetBottom(){
+        return _bottom;
+    }
+    public void SetBottom(int bottom){
+        _bottom = bottom;
+    }
+    public string GetFractionString(){
+        return _top + "/" + _bottom;
+    }
+    public double GetDecimalValue(){
+        Console.WriteLine(_top / _bottom);
+        Console.WriteLine(_top);
+        Console.WriteLine(_bottom);
+        return (double) _top / (double)_bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction num = new Fraction();
+        Fraction num1 = new Fraction(6);
+        Fraction num2 = new Fraction(6,7);
+        Console.WriteLine(num.GetFractionString());
+        Console.WriteLine(num.GetDecimalValue());
+        Console.WriteLine(num1.GetFractionString());
+        Console.WriteLine(num1.GetDecimalValue());
+        Console.WriteLine(num2.GetFractionString());
+        Console.WriteLine(num2.GetDecimalValue());
     }
+
+
 }
